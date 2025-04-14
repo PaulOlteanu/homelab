e2e0ba69b804b6eb5abc773d8e583d7f895f31ac
Paul Olteanu <p.a.olteanu@gmail.com>
2025-04-13 20:38:00 -0400

    it's alive

--- configurations/nixos-1.nix
+++ configurations/nixos-1.nix
@@ -8,12 +8,17 @@
     networking.hostName = "nixos-1";
 
     networking.firewall.allowedTCPPorts = [
+      80
+      443
       6443 # k32 API server
       2379 # k3s etcd clients
       2380 # k3s etcd peers
+      7946 # metallb
     ];
     networking.firewall.allowedUDPPorts = [
       8437 # k3s flannel
+      7946 # metallb
+      8472 # metallb
     ];
 
     services.k3s = {
@@ -22,6 +27,7 @@
       clusterInit = true;
       extraFlags = [
         "--disable traefik"
+        "--disable servicelb"
       ];
     };
 
--- configurations/nixos-3.nix
+++ configurations/nixos-3.nix
@@ -8,12 +8,17 @@
     networking.hostName = "nixos-3";
 
     networking.firewall.allowedTCPPorts = [
+      80
+      443
       6443 # k32 API server
       2379 # k3s etcd clients
       2380 # k3s etcd peers
+      7946 # metallb
     ];
     networking.firewall.allowedUDPPorts = [
       8437 # k3s flannel
+      7946 # metallb
+      8472 # metallb
     ];
 
     services.k3s = {
@@ -22,6 +27,7 @@
       tokenFile = "/etc/k3s-token";
       extraFlags = [
         "--disable traefik"
+        "--disable servicelb"
       ];
       serverAddr = "https://192.168.0.187:6443";
     };
